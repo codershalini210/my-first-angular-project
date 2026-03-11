974f1bb5fa58ba0bf34eb8d5e1ee02dd8869d45a
shalini <s.rathore43@gmail.com>
2026-03-11 19:27:52 +0530

    kjhk

--- src/app/timer/timer.ts
+++ src/app/timer/timer.ts
@@ -9,23 +9,58 @@ import {CommonModule} from "@angular/common"
 export class Timer {
 minutes:number
 seconds:number
+  timeLeftColor: string;
+buttonLabel: string;
+  isPaused: boolean;
 constructor(private cd: ChangeDetectorRef )
 {
-  this.minutes =1
-  this.seconds=59
-  setInterval(()=>this.tick(),1000)
+   this.buttonLabel = 'Start';
+  this.isPaused = false;
+   this.minutes = 3;
+    this.seconds = 59;
+  this.timeLeftColor = 'haveTime';
+  setInterval(()=>this.tick(),200)
+  
+    // setInterval(() => this.tick(), 1000);
 }
 tick()
 {
-   
+    setInterval(() => this.timeLeft(), 200);
+     if (!this.isPaused) {
+      console.log("here")
+      this.buttonLabel = 'Pause';
   if (--this.seconds < 0) {
       this.seconds = 59;
       if (--this.minutes < 0) {
-        this.minutes = 1;
-        this.seconds = 59;
-      }
-   
+   this.reset();
+     }
   }
+}
 this.cd.detectChanges();
 }
+
+ timeLeft(): void {
+    if (this.minutes >= 2) {
+      this.timeLeftColor = 'haveTime';
+      return;
+    }
+    if (this.minutes < 2 && this.minutes >= 1) {
+      this.timeLeftColor = 'lessTime';
+      return;
+    }
+    this.timeLeftColor = 'noTime';
+  }
+  ngOnInit(): void {
+  }
+
+
+   togglePause() {
+    this.isPaused = !this.isPaused;
+    this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+  }
+
+   reset(): void {
+    this.minutes = 3;
+    this.seconds = 59;
+  }
 }
